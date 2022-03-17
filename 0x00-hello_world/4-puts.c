108fbabddcb62141c591e1c52bf9c41b0339d881
Hiuhuedd <hiuhuedd@gmail.com>
2022-03-17 05:02:43 -0700

    corrections

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,13 @@
 #include<stdio.h>
+
 /**
-* main -Where programs are execute`d
+* main -Where programs are executed
+*
 * return: 0
 */
+
 int main(void)
+
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
