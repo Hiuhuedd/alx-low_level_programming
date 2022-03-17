944c5d1a0fafe4506a5604a6d6a1d76ce67ae16a
Hiuhuedd <hiuhuedd@gmail.com>
2022-03-17 06:15:06 -0700

    corrections

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,7 +17,7 @@ int main(void)
 
 	printf("Size of char:%lu\n",(unsigned long)sizeof(c));
 	printf("Size of int:%lu\n",(unsigned long)sizeof(a));
-	printf("Size of long long int:%lu\n",(unsigned long)sizeof(d));
 	printf("Size of long int:%lu\n",(unsigned long)sizeof(b));
+	printf("Size of long long int:%lu\n",(unsigned long)sizeof(d));
 	printf("Size of float:%lu\n",(unsigned long)sizeof(f));
 }
