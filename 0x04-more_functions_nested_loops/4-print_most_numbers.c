cbacfbf61e1af7f85918e52eebf3d1db46d4bfa7
Hiuhuedd <hiuhuedd@gmail.com>
2022-03-18 03:39:24 -0700

    more functions and nested loops

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,15 +8,15 @@
 
 void print_most_numbers(void)
 {
-char c = '0';
-while (c <= '9')
-{
-if (c != '2' && c != '4')
-{
-_putchar(c);
-}
-c++;
-}
-putchar('\n');
+	char c = '0';
+	while (c <= '9')
+	{
+	if (c != '2' && c != '4')
+	{
+	_putchar(c);
+	}
+	c++;
+	}
+	_putchar('\n');
 	
 }
