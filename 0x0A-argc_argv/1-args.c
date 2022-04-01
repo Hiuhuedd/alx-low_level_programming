dae5d8a7e2a98363296b4670cddca72c4a1958d0
Hiuhuedd <hiuhuedd@gmail.com>
2022-04-01 11:26:42 -0700

    argument count and vectors

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the number of arguments passed
+ *
+ * argc - offset count
+ * argv - offset vector
+ *
+ * return 0
+ */
+
+int main(int argc char *argv[])
+{
+	printf("%s\n",argv[0]);
+	return 0;
+}
